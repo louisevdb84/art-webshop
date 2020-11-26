5dfbe4976218d72bc61772afcb90876839d698b6
louisevdb84 <louisevdb84@gmail.com>
2020-11-26 16:13:44 +0100

    Updates to use users

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './webshop/redux/store';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+     <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/webshop/app-webshop.tsx
+++ src/webshop/app-webshop.tsx
@@ -1,25 +1,78 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { Container } from 'react-bootstrap';
 import WebshopNavigation from './webshop-navigation';
 import Webshop from './pages/webshop.component'
 
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
+import { createStructuredSelector } from 'reselect';
 
-function App() {
-  return (
-    <div className="App">
-       <WebshopNavigation></WebshopNavigation>
-        <Container>
 
-          <BrowserRouter>
-            <Switch>              
-              <Route exact path="/art-webshop/webshop" component={Webshop} />                               
-            </Switch>
-            </BrowserRouter>
-          </Container>
-     
-    </div>
-  );
+class AppWebshop extends React.Component {
+  unsubscribeFromAuth = null;
+
+  // componentDidMount() {
+  //   const { setCurrentUser } = this.props;
+
+  //   this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+  //     if (userAuth) {
+  //       const userRef = await createUserProfileDocument(userAuth);
+
+  //       userRef.onSnapshot(snapShot => {
+  //         setCurrentUser({
+  //           id: snapShot.id,
+  //           ...snapShot.data()
+  //         });
+  //       });
+  //     }
+
+  //     setCurrentUser(userAuth);
+  //   });
+  // }
+
+  // componentWillUnmount() {
+  //   this.unsubscribeFromAuth();
+  // }
+
+  render() {
+    return (
+      <div>
+        <WebshopNavigation></WebshopNavigation>
+        <Switch>
+        <Route exact path="/art-webshop/webshop" component={Webshop} />   
+          {/* <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} /> */}
+          {/* <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          /> */}
+        </Switch>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
+});
+
+// const mapDispatchToProps = dispatch => ({
+//   setCurrentUser: user => dispatch(setCurrentUser(user))
+// });
+
+export default connect(
+  mapStateToProps,
+  // mapDispatchToProps
+)(AppWebshop);
+
